8c6b8f0856fdab9c3e3a2f3f18657004ff16d216
user.name like:"psiddhant <psiddhant>
2024-08-16 23:23:24 +0530

    ad

--- src/app/termscondition/page.jsx
+++ src/app/termscondition/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-        <Image src={"/policy.png"} height={700} width={700}/>
+        <Image src={"/policy.png"} height={750} width={750}/>
     </div>
   )
 }
