24e63606af22104e99701f0276c5b409fe981468
user.name like:"psiddhant <psiddhant>
2024-08-16 23:55:49 +0530

    dsd

---
+++ src/app/refundpolicy/page.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+         <div className='h-[2000px] mb-44 mt-64'>
+   <h1 className='text-3xl ml-9 mt-10  mb-12 '>
+   Cancellation & Refund Policy
+      </h1>
+      <p className='ml-14 mt-4'>Last updated on 16-08-2024 23:20:26      </p>
+      <p className='ml-14 mt-6'>JAIVIKHABITAT FARMER HORICULTURE PRIVATE LIMITED believes in helping its customers
+      as far as possible, and has therefore a liberal cancellation policy. Under this policy:</p>
+     <ul className='mt-6 ml-14'>
+     <li classname="ml-14 mb-5 pb-6">Cancellations will be considered only if the request is made immediately after placing the order.
+However, the cancellation request may not be entertained if the orders have been communicated to the
+vendors/merchants and they have initiated the process of shipping them</li>
+        <li classname="ml-14 mb-5 pb-6 mt-2">JAIVIKHABITAT FARMER HORICULTURE PRIVATE LIMITED does not accept cancellation
+requests for perishable items like flowers, eatables etc. However, refund/replacement can be made if the
+customer establishes that the quality of product delivered is not good.
+
+        </li>
+        <li classname="ml-14 mb-5 pb-6">In case of receipt of damaged or defective items please report the same to our Customer Service team.
+The request will, however, be entertained once the merchant has checked and determined the same at his
+own end. This should be reported within 15 Days days of receipt of the products. In case you feel that the
+product received is not as shown on the site or as per your expectations, you must bring it to the notice of
+our customer service within 15 Days days of receiving the product. The Customer Service Team after
+looking into your complaint will take an appropriate decision.
+        </li>
+        <li classname="ml-14 mb-5 pb-6"> In case of complaints regarding products that come with a warranty from manufacturers, please refer
+the issue to them. In case of any Refunds approved by the JAIVIKHABITAT FARMER
+HORICULTURE PRIVATE LIMITED, it’ll take 9-15 Days days for the refund to be processed to the
+end customer.
+        </li>
+     </ul>
+   </div>
+    </div>
+  )
+}
+
+export default page
